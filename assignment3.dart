1e8f82241f264ade3f4d2268b5de9c4e18965c33
MAmirK <muhammadamirk820@gmail.com>
2024-06-23 20:10:01 +0500

    assignment3

---
+++ assignment3.dart
@@ -0,0 +1,25 @@
+void main() {
+  List<String> days = [
+    'MONDAY',
+    'TUESDAY',
+    'WEDNESDAY',
+    'THURSDAY',
+    'FRIDAY',
+    'SATURDAY',
+    'SUNDAY'
+  ];
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+}
