3d43f7d033ec056cd32daecab98c815c81629cda
MAmirK <muhammadamirk820@gmail.com>
2024-06-30 18:33:23 +0500

    assignment12

---
+++ assignment12.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> originalList = ['ali', 'saad', 'ammar', 'amir'];
+  List<String> reversedList = originalList.reversed.toList();
+  print(reversedList);
+  print(originalList);
+}
